d88c759a6cd02e0f52efda5c6086f5f60d3f9d09
Mateja Radojicic <mateja@xiitech.org>
2019-04-18 19:50:39 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,43 @@
 <?php
-
-echo "index";
-
-
+session_start();
+if (!isset($_SESSION['email'])) {
+    header("location:login.php");
+}
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+    <meta charset="utf-8">
+    <title>Login</title>
+
+	<link href="css/reset.css" rel="stylesheet" media="screen">
+	<link href="css/index.css" rel="stylesheet" media="screen">	
+	
+	<div class="navbar-capsule">
+		<div class="logoDiv">
+			<div class="logo-capsule"><a href="#" class="logoAtag">Alpha</a> by HTML5 UP</div>
+		</div>
+	
+		<div class="buttonDiv">	
+			<div class="buttons-capsule">
+				<button class="navbar-button leftEdge">Home</button><button class="navbar-button">Layaouts</button><button class="navbar-button rightEdge">Sign Up</button>	
+			</div>
+		</div>
+	</div>
+	</head>
+
+<body>
+<div class="wallpaper"></div>
+<div class="back1">
+<p class="back1Inside">
+123
+</p>
+</div>
+<div class="back2">123</div>
+
+
+</body>
+</html>
+
